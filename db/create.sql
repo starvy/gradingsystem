84483d90ab15ab1d468c9805a29c3d5c204bf10d
starvy <sebastian.pravda@gmail.com>
2022-07-16 10:58:24 +0200

    skip tests, db, db gen

--- db/create.sql
+++ db/create.sql
@@ -21,13 +21,3 @@ alter table if exists TimeSlot add constraint FKrh0c8mn30597w26krh1k61g3b foreig
 alter table if exists TimeSlot add constraint FKgumuf5m1mr2qtbdw9jufk394o foreign key (subject_id) references t_subjects;
 alter table if exists TimeTable_TimeSlot add constraint FKdns8knoqxsohdokejxnxt4fad foreign key (timeSlots_id) references TimeSlot;
 alter table if exists TimeTable_TimeSlot add constraint FKru0idhjs6rj2vo3o8y65gfbix foreign key (TimeTable_id) references TimeTable;
-INSERT INTO public.t_users (id, email, fullname, password, role, username) VALUES (1, 'debil@gmail.com', '', '$2a$05$Jrq2TDwLUIPFo5Yda8G8neJrd1DdtDjOJjnaRcI9aJ132QUGOLesK', 'USER', 'debil');
-INSERT INTO public.t_users (id, email, fullname, password, role, username) VALUES (2, 'blatny@blatny.cz', 'Honza', '$2a$12$mlEnv1aMgHvafYCNOBG4N.KUYTS.SLFZtYfnXedUPmJfgeyIlsBKu', 'SUPERADMIN', 'superadmin');
-INSERT INTO public.t_grades (id, description, title, value, student_id) VALUES (1, '', 'ahoj', 2, 1);
-INSERT INTO public.t_grades (id, description, title, value, student_id) VALUES (2, '', 'ahoj', 2, 1);
-INSERT INTO public.t_grades (id, description, title, value, student_id) VALUES (3, '', 'ahoj', 2, 1);
-INSERT INTO public.t_grades (id, description, title, value, student_id) VALUES (4, '', 'ahoj', 2, 1);
-INSERT INTO public.t_grades (id, description, title, value, student_id) VALUES (5, '', 'ahoj', 2, 1);
-INSERT INTO public.t_grades (id, description, title, value, student_id) VALUES (6, '', 'ahoj', 2, 1);
-INSERT INTO public.t_grades (id, description, title, value, student_id) VALUES (7, '', 'ahoj', 2, 1);
-INSERT INTO public.t_grades (id, description, title, value, student_id) VALUES (8, '', 'ahoj', 2, 1);
